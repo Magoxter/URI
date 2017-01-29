9813eb92576135acc8826c6ca1658a2b20b7edda
Magoxter <will.lbx@hotmail.com>
2017-01-29 20:16:32 -0300

    Media 1

--- 1005.c
+++ 1005.c
@@ -3,15 +3,15 @@
 
 int main (void) {
   float nota1,
-  		nota2,
-  		notaA,
-  		notaB,
-  		media;
-  	scanf("%f", &nota1);
-  	scanf("%f", &nota2);
+  	nota2,
+  	notaA,
+  	notaB,
+  	media;
+    scanf("%f", &nota1);
+    scanf("%f", &nota2);
   notaA = nota1 * 3.5;
   notaB = nota2 * 7.5;
   media = (notaA + notaB) / 11.0;
-  	printf("MEDIA = %.5f\n", media);
+   printf ("MEDIA = %.5f\n", media);
  return 0;
 }
