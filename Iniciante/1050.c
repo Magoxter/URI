5b9b9fcdb8cdcd46b59d7eaefcd41f4dab8190d1
Will Xavier <will.lbx@hotmail.com>
2017-03-10 15:14:32 -0400

    DDD

---
+++ Iniciante/1050.c
@@ -0,0 +1,37 @@
+	/* DDD */
+#include <stdio.h>
+
+enum {
+	Campinas = 19,
+	Paulo = 11,
+	Rio = 21,
+	Vitoria = 27,
+	Horizonte = 31,
+	Juiz = 32,
+	Brasilia = 61,
+	Salvador = 71
+} Estado;
+
+int main (void) {
+  int Estado;
+  	scanf("%d", &Estado);
+  if (Estado == Paulo)
+  	printf("Sao Paulo\n");
+  else if (Estado == Brasilia)
+  	printf("Brasilia\n");
+  else if (Estado == Salvador)
+  	printf("Salvador\n");
+  else if (Estado == Rio)
+  	printf("Rio de Janeiro\n");
+  else if (Estado == Juiz)
+  	printf("Juiz de Fora\n");
+  else if (Estado == Campinas)
+  	printf("Campinas\n");
+  else if (Estado == Vitoria)
+  	printf("Vitoria\n");
+  else if (Estado == Horizonte)
+  	printf("Belo Horizonte\n");
+  else
+  	printf("DDD nao cadastrado\n");
+ return 0;
+}
