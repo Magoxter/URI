542fce10144631dd6ab79a3b7f2cf129239d26b1
Will Xavier <will.lbx@hotmail.com>
2017-03-13 13:10:20 -0400

    Criptografia

---
+++ Strings/1024.c
@@ -0,0 +1,42 @@
+	/* Criptografia */
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+
+int main (void) {
+  char txt[MAX];
+  int  n,i,r,k,
+       tmp,f,m,
+       ctrl,t;
+   scanf("%d", &r);
+ for (k = 0; k < r; k++) {
+ 	scanf(" %[^\n]%*c", txt);
+   n = strlen(txt);
+   ctrl = 0;
+  for (i = 0; i < n; i++) {
+    if (txt[i] > 64 && txt[i] < 91)
+      ctrl++;
+    else if (txt[i] > 96 && txt[i] < 123)
+      ctrl++;
+    if (ctrl)
+      txt[i]+=3;
+      ctrl = 0;
+ }  f = n-1;
+    m = f/2;
+    i = 0;
+  while (i <= m) {
+   tmp = txt[i];
+   txt[i] = txt[f];
+   txt[f] = tmp;
+   f--; i++;  }
+   f = n-1;
+   m = f/2;
+   t = 0;
+  if (n %2 == 0)
+    t = 1;
+  for (i = m+t; i < n; i++)
+    txt[i]--;
+   printf("%s\n", txt);
+ }
+ return 0;
+}
