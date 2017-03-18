b061a385fe533840aecfeba24ecffb5f767db56d
Will Xavier <will.lbx@hotmail.com>
2017-03-17 22:49:22 -0400

    Numeros Impares

---
+++ Iniciante/1067.c
@@ -0,0 +1,11 @@
+    /* Impares */
+#include <stdio.h>
+
+int main (void) {
+  int n,i;
+    scanf("%d", &n);
+   for (i = 0; i < n+1; i++)
+    if (i %2 != 0)
+      printf("%d\n", i);
+ return 0;
+}
