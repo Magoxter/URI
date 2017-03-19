6f5c59aeba6cf1ce76b065cf13ae272ad6223c42
Will Xavier <will.lbx@hotmail.com>
2017-03-19 17:58:11 -0400

    Numero Perfeito

---
+++ Iniciante/1164.c
@@ -0,0 +1,21 @@
+    /* Num Perfeito */
+#include <stdio.h>
+
+int main (void) {
+  int n,i,k,
+      repet,
+      soma;
+    scanf("%d", &repet);
+ for (k = 0; k < repet; k++) {
+     soma = 0;
+    scanf("%d", &n);
+   for (i = 1; i < n; i++)
+     if (n % i == 0)
+        soma += i;
+  if (soma == n)
+    printf("%d eh perfeito\n", n);
+  else
+    printf("%d nao eh perfeito\n", n);
+ }
+ return 0;
+}
