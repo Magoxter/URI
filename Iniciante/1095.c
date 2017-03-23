b55a5a48f0a5017088b84cb866613bb8c32677f5
Will Xavier <will.lbx@hotmail.com>
2017-03-23 19:23:08 -0400

    Sequencia IJ 1

---
+++ Iniciante/1095.c
@@ -0,0 +1,14 @@
+    /* Seq IJ 1 */
+#include <stdio.h>
+
+int main (void) {
+  int i,j;
+   i = -2;
+   j = 65;
+ while (j-5 >= 0) {
+   i+=3;
+   j-=5;
+  printf("I=%d J=%d\n", i, j);
+ }
+ return 0;
+}
