f421a47c58a612860c6267600eb6da33349c1c41
Will Xavier <will.lbx@hotmail.com>
2017-04-09 02:52:20 -0400

    Gasto de Combustivel

--- 000/1017.c
+++ 000/1017.c
@@ -4,9 +4,9 @@
 int main (void) {
   float litros;
   int	quilometragem,
-  		horas,distancia;
-  	scanf("%d", &horas);
-  	scanf("%d", &quilometragem);
+        horas,distancia;
+    scanf("%d", &horas);
+    scanf("%d", &quilometragem);
   distancia = quilometragem * horas;
   litros = distancia / 12.0;
    printf("%.3f\n", litros);
