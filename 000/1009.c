d41326d92fa7e9b9a0e5c793c1f1de358db904f6
Will Xavier <will.lbx@hotmail.com>
2017-04-09 02:42:40 -0400

    Salario com Bonus

--- 000/1009.c
+++ 000/1009.c
@@ -1,4 +1,4 @@
-	/* Salario Bonificado */
+/* Salario Bonificado */
 #include <stdio.h>
 #include <string.h>
 #define MAX 1000
@@ -8,12 +8,12 @@ typedef char string[MAX];
 int main (void) {
   string vendedor;
   double total,
-  		 vendas,
-  		 salario,
-  		 comissao;
-  	scanf("%s", vendedor);
-  	scanf("%lf", &salario);
-  	scanf("%lf", &vendas);
+         vendas,
+         salario,
+         comissao;
+    scanf("%s", vendedor);
+    scanf("%lf", &salario);
+    scanf("%lf", &vendas);
   comissao = vendas * 0.15;
   total = salario + comissao;
    printf("TOTAL = R$ %.2lf\n", total);
