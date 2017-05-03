902b9c2ed76ed10a95a96a93a622e062f847088a
Will Xavier <will.lbx@hotmail.com>
2017-05-03 13:26:47 -0400

    Blobs

---
+++ 100/1170.c
@@ -0,0 +1,17 @@
+	/* Blob */
+#include <stdio.h>
+
+int main (void) {
+  float sup;
+  int 	r,d;
+    scanf("%d", &r);
+ while (r--) {
+   scanf("%f", &sup);
+   d = 0;
+  while(sup > 1.0) {
+  	sup /= 2.0;
+   d++;
+  } printf("%d dias\n", d);
+ }
+ return 0;
+}
