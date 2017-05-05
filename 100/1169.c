ee7237cc8e03526b447de503d6dfcc05a78902cf
Will Xavier <will.lbx@hotmail.com>
2017-05-05 13:25:38 -0400

    Create 1169.c

---
+++ 100/1169.c
@@ -0,0 +1,21 @@
+	/* Tabuleiro */
+#include <stdio.h>
+
+int main (void) {
+  double grao;
+  long long
+  int 	 kg;
+  int    quad,r;
+   scanf("%d", &r);
+ while (r--) {
+   scanf("%d", &quad);
+   grao = 1.0;
+  while (quad--) {
+   grao *= 2.0;
+  } grao /= 12.0;
+   grao /= 1000.0;
+   kg = grao;
+  printf("%lld kg\n", kg);
+ }
+ return 0;
+}
