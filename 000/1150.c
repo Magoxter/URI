be534b6ce3e391532854f2558d8c191c2546bdbd
Will Xavier <will.lbx@hotmail.com>
2017-05-19 13:53:38 -0400

    Ultrapassando Z

---
+++ 000/1150.c
@@ -0,0 +1,17 @@
+    /* Ultrapassing Z */
+#include <stdio.h>
+
+int main (void) {
+  int k,y,x,
+      sum;
+   scanf("%d", &x);
+   scanf("%d", &y);
+ while (y <= x)
+  scanf("%d", &y);
+  k = 0;
+ while (sum < y) {
+   sum += x++;
+  k++;
+ } printf("%d\n", k);
+ return 0;
+}
