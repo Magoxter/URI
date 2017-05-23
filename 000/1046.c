5b9544f0f2dec254c6ce6549321013524555c4f5
Will Xavier <will.lbx@hotmail.com>
2017-05-22 22:20:43 -0400

    Tempo de Jogo

--- 000/1046.c
+++ 000/1046.c
@@ -5,18 +5,18 @@ void troca (int*,int*);
 
 int main (void) {
   int hora1,
-  	  horaF,
-  	  tempo;
+      horaF,
+      tempo;
 
-  	scanf("%d", &hora1);
-  	scanf("%d", &horaF);
+    scanf("%d", &hora1);
+    scanf("%d", &horaF);
 
   if (hora1 > horaF)
-  	tempo = (24 - hora1) + horaF;
+    tempo = (24 - hora1) + horaF;
   if (hora1 == horaF)
-  	tempo = 24;
+    tempo = 24;
   if (hora1 < horaF)
-  	tempo = horaF - hora1;
+    tempo = horaF - hora1;
 
   printf("O JOGO DUROU %d HORA(S)\n", tempo);
 
@@ -25,8 +25,8 @@ int main (void) {
 
 void troca (int *x, int *y) {
   int tmp;
-  	tmp = *x;
-  	*x = *y;
-  	*y = tmp;
+   tmp = *x;
+   *x = *y;
+   *y = tmp;
  return;
 }
