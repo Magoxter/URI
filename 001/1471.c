9fa7d9b6a268df77e9e6180fb267be37fb28f93a
Will Xavier <will.lbx@hotmail.com>
2017-09-19 12:24:33 -0400

    Mergulho

--- 001/1471.c
+++ 001/1471.c
@@ -1,12 +1,13 @@
 	/* Ret */
 #include <stdio.h>
-#define MAX 10000
+#define MAX 1000
 
 void ordena(int,int[]);
+void troca (int*,int*);
 
 int main (void) {
-  int n,i,r,k,
-      ret[MAX];
+  int n,i,r,k;
+	int ret[MAX];
  while (scanf("%d", &n) != EOF) {
     scanf("%d", &r);
    for (i = 0; i < r; i++)
@@ -14,7 +15,6 @@ int main (void) {
    if (r == n)
      printf("*\n");
    else {
-     ordena(r,ret);
      ordena(r,ret);
      k = 1;
     for (i = 0; i < r; i++) {
@@ -25,25 +25,30 @@ int main (void) {
     } if (k <= n)
      for (i = k; i <= n; i++)
        printf("%d ", i);
-       printf("\n");
+      printf("\n");
    }
  }
  return 0;
 }
 
 void ordena (int n, int v[MAX]) {
-  int i,j,
-      min,
-      tmp;
-    for (i = 0; i < n-1; i++) {
-        min = i;
-       for (j = i+1; j < n; j++) {
-          if (v[j] < v[min]) {
-             min = j;
-              tmp = v[i];
-              v[i] = v[min];
-              v[min] = tmp;
-          }
-       }
-    }
+  int k,i;
+  int min;
+ for (i = 0; i < n-1; i++) {
+    min = i;
+  for (k = i+1; k < n; k++) {
+   if (v[k] < v[min])
+    min = k;
+  } if (min != i)
+      troca(&v[i],&v[min]);
+ }
+ return;
 }
+
+void troca (int *x, int *y) {
+  int ax;
+   ax = *y;
+   *y = *x;
+   *x = ax;
+ return;
+} 
